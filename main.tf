56a50b18f4ad1ecb9995e765cdb5011f3e852ff4
panhavathGit <158982815+panhavathGit@users.noreply.github.com>
2024-12-30 01:10:04 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,33 @@
-terraform { 
-    backend "remote" {
-      organization = "vath_organization"
-      workspaces{
-        name = "Example-Workspace"
-      }
+terraform {
+  backend "remote" {
+    organization = "vath_organization"
+    workspaces {
+      name = "Example_Workspace"
     }
-    
-    required_providers { 
-        aws = { 
-        source  = "hashicorp/aws" 
-        version = "~> 4.16" 
-        } 
-    } 
-    required_version = ">= 1.2.0" 
-} 
+  }
 
-provider "aws" { 
-region  = "us-west-2"
-} 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
+  ami           = "ami-07d9cf938edb0739b"
   instance_type = "t2.micro"
-
+  key_name      = "project"  
   tags = {
-    Name = var.instance_name
+    Name = "my-instance"
   }
 }
+
